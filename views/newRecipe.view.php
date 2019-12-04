d3fa1f30ed6e1226f7f894e35f8d1b9f9aef66b6
alicelindholm <54351014+alicelindholm@users.noreply.github.com>
2019-12-04 19:53:12 +0100

    ingredienser + instruktioner

--- views/newRecipe.view.php
+++ views/newRecipe.view.php
@@ -9,60 +9,59 @@
 </head>
 <body>
 <h1> Nytt recept </h1>
-<form action="../storeRecipe.php" method="post">
+<form action="storeRecipe.php" method="post">
     <label for="name">Namn: </label>
     <input type="text" name="name" id="name">
     <br>
     <h3>Ingredienser</h3>
     <p> Glöm inte att fylla i mängd!</p>
-    <?php
-    $amountIngredients = 5;
-    for($i=0; $i<$amountIngredients; $i++){ ?>
-        <label> Ingrediens <?=$i+1?>:
-        <input type="text" name="ingredient_<?=$i+1?>" id="<?=$i+1?>">
-        </label>
-        <br>
-    <?php }
-    ?>
-    <p><?php echo $amountIngredients;?></p>
-    <form action="<?=$amountIngredients+=1;?>">
-
-    <input class="plus" value="+" type="button">
-    </form>
-    <hr>
+    <div id="ingredientBox">
+        <?php
+        for ($i = 0; $i < 5; $i++) { ?>
+            <label> Ingrediens <?= $i + 1 ?>:
+                <input type="text" name="ingredient_<?= $i + 1 ?>" id="<?= $i + 1 ?>">
+            </label>
+            <br>
+        <?php }
+        ?>
+    </div>
+    <input type="button" onclick="addIngredient()" value="+"> <label for=""> Lägg till ingrediens</label>
     <h3>Instruktioner</h3>
-    <label for="instruction_1">Steg 1: </label> <br>
-    <textarea id="instruction_1" name="instruction_1" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_2">Steg 2: </label> <br>
-    <textarea id="instruction_2" name="instruction_2" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_3">Steg 3: </label> <br>
-    <textarea id="instruction_3" name="instruction_3" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_4">Steg 4: </label> <br>
-    <textarea id="instruction_4" name="instruction_4" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_5">Steg 5: </label> <br>
-    <textarea id="instruction_5" name="instruction_5" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_6">Steg 6: </label> <br>
-    <textarea id="instruction_6" name="instruction_6" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_7">Steg 7: </label> <br>
-    <textarea id="instruction_7" name="instruction_7" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_8">Steg 8: </label> <br>
-    <textarea id="instruction_8" name="instruction_8" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_9">Steg 9: </label> <br>
-    <textarea id="instruction_9" name="instruction_9" rows="10" cols="70"></textarea>
-    <br>
-    <label for="instruction_10">Steg 10: </label> <br>
-    <textarea id="instruction_10" name="instruction_10" rows="10" cols="70"></textarea>
+    <div id="instructionBox">
+        <?php
+        for ($i = 0; $i < 5; $i++) { ?>
+            <label> Steg <?= $i + 1 ?>: </label> <br>
+            <textarea name="instruction_<?= $i + 1 ?>" id="instruction_<?= $i + 1 ?>" rows="10" cols="70"></textarea>
+
+            <br>
+        <?php }
+        ?>
+    </div>
+    <input type="button" onclick="addInstruction()" value="+"> <label for="">Lägg till instruktion</label>
     <br>
     <input type="submit" value="Lägg till recept">
 
 </form>
+<script>
+    let amountIngredient = 5;
+    let amountInstruction = 5;
+
+    function addIngredient() {
+        amountIngredient += 1;
+        if (amountIngredient < 11) {
+            document.getElementById("ingredientBox").innerHTML += "<label>Ingrediens " + amountIngredient + ": <input type='text' name='ingredient_" + amountIngredient + "' id='ingredient_" + amountIngredient + "' ></label> <br>";
+        } else {
+            alert("Du kan inte ha mer än 10 ingredienser!")
+        }
+    }
+    function addInstruction() {
+        amountInstruction += 1;
+        if (amountInstruction < 11) {
+            document.getElementById("instructionBox").innerHTML += "<label>Steg " + amountInstruction + ":</label><br> <textarea type='text' name='instruction_" + amountInstruction + "' id='instruction_" + amountInstruction + "'  rows=\"10\" cols=\"70\"></textarea> <br>";
+        } else {
+            alert("Du kan inte ha mer än 10 instruktioner!")
+        }
+    }
+</script>
 </body>
 </html>
