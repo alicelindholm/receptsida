417100ba3e657f64e13701ad80a9b68b2a4b2974
alicelindholm <54351014+alicelindholm@users.noreply.github.com>
2020-01-08 09:54:13 +0100

    så mycket

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @return PDO
+ */
 function connect()
 {
     try {
@@ -13,23 +16,10 @@ function connect()
     }
 }
 
-function runQuery($query)
-{
-    $db = connect();
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    return "success";
-}
 
-function fetchAll($query)
-{
-    $db = connect();
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-    return $result;
-}
-
+/**
+ * @param $getId
+ */
 function deleteRecipe($getId)
 {
 
@@ -41,12 +31,100 @@ function deleteRecipe($getId)
     $query3 = "DELETE FROM instructions WHERE id = '$id'";
 
     unlink($file);
-
     runQuery($query1);
     runQuery($query2);
     runQuery($query3);
 }
 
+
+/**
+ * @param $query
+ * @return array
+ */
+function fetchAll($query)
+{
+    $db = connect();
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $result;
+}
+
+/**
+ * @return array
+ */
+function getAll()
+{
+    return $result = array(getAllNames(), getAllIngredients(),
+        getAllInstructions());
+}
+
+
+/**
+ * @return array
+ */
+function getAllIngredients()
+{
+    $query = "SELECT * FROM ingredients WHERE 1";
+    return fetchAll($query);
+}
+
+/**
+ * @return array
+ */
+function getAllInstructions()
+{
+    $query = "SELECT * FROM instructions WHERE 1";
+    return fetchAll($query);
+}
+
+
+/**
+ * @return array
+ */
+function getAllNames()
+{
+    $query = "SELECT * FROM recipes WHERE 1";
+    return fetchAll($query);
+}
+
+
+/**
+ * @param $id
+ * @return array
+ */
+function getOneIngredients($id)
+{
+    $query = "SELECT * FROM ingredients WHERE id = $id";
+    return fetchAll($query);
+}
+
+
+/**
+ * @param $id
+ * @return array
+ */
+function getOneInstructions($id)
+{
+    $query = "SELECT * FROM instructions WHERE id = $id";
+    return fetchAll($query);
+}
+
+
+/**
+ * @param $id
+ * @return array
+ */
+function getOneName($id)
+{
+    $query = "SELECT * FROM recipes WHERE id = $id";
+    return fetchAll($query);
+}
+
+/**
+ * @param $id
+ * @return array
+ */
 function getOneRecipe($id)
 {
     return $result = array(getOneName($id), getOneIngredients($id),
@@ -54,167 +132,186 @@ function getOneRecipe($id)
 
 }
 
-function getAll()
+
+/**
+ * @param $query
+ */
+function runQuery($query)
 {
-    return $result = array(getAllNames(), getAllIngredients(),
-        getAllInstructions());
+    $db = connect();
+    $stmt = $db->prepare($query);
+    $stmt->execute();
 }
 
-function getAllNames()
-{
-    $query = "SELECT * FROM recipes WHERE 1";
-    return fetchAll($query);
-}
-
-function getAllIngredients()
-{
-    $query = "SELECT * FROM ingredients WHERE 1";
-    return fetchAll($query);
-}
-
-function getAllInstructions()
-{
-    $query = "SELECT * FROM instructions WHERE 1";
-    return fetchAll($query);
-}
-
-function getOneName($id)
-{
-    $query = "SELECT * FROM recipes WHERE id = $id";
-    return fetchAll($query);
-}
-
-function getOneIngredients($id)
-{
-    $query = "SELECT * FROM ingredients WHERE id = $id";
-    return fetchAll($query);
-}
-
-function getOneInstructions($id)
-{
-    $query = "SELECT * FROM instructions WHERE id = $id";
-    return fetchAll($query);
-}
-
-function updateRecipe($id, $data, $img)
-{
-    var_dump($id);
-    var_dump($data);
-    var_dump($img);
-    $name = $data["name"];
-    $ingredient_1 = $data['ingredient_1'] ?? null;
-    $ingredient_2 = $data['ingredient_2'] ?? null;
-    $ingredient_3 = $data['ingredient_3'] ?? null;
-    $ingredient_4 = $data['ingredient_4'] ?? null;
-    $ingredient_5 = $data['ingredient_5'] ?? null;
-    $ingredient_6 = $data['ingredient_6'] ?? null;
-    $ingredient_7 = $data['ingredient_7'] ?? null;
-    $ingredient_8 = $data['ingredient_8'] ?? null;
-    $ingredient_9 = $data['ingredient_9'] ?? null;
-    $ingredient_10 = $data['ingredient_10'] ?? null;
-    $instruction_1 = $data['instruction_1'] ?? null;
-    $instruction_2 = $data['instruction_2'] ?? null;
-    $instruction_3 = $data['instruction_3'] ?? null;
-    $instruction_4 = $data['instruction_4'] ?? null;
-    $instruction_5 = $data['instruction_5'] ?? null;
-    $instruction_6 = $data['instruction_6'] ?? null;
-    $instruction_7 = $data['instruction_7'] ?? null;
-    $instruction_8 = $data['instruction_8'] ?? null;
-    $instruction_9 = $data['instruction_9'] ?? null;
-    $instruction_10 = $data['instruction_10'] ?? null;
-
-    $query1 = "UPDATE recipes SET 
-                   name='$name',
-                   img = '$img'
-            WHERE id = $id";
-    $query2 = "UPDATE ingredients SET
-                       ingredient_1 = '$ingredient_1',
-    ingredient_2= '$ingredient_2',
-    ingredient_3= '$ingredient_3',
-    ingredient_4= '$ingredient_4',
-    ingredient_5= '$ingredient_5',
-    ingredient_6= '$ingredient_6',
-    ingredient_7= '$ingredient_7',
-    ingredient_8= '$ingredient_8',
-    ingredient_9= '$ingredient_9',
-    ingredient_10 = '$ingredient_10'
-                       WHERE id = $id";
-    $query3 = "UPDATE instructions SET
-    instruction_1 = '$instruction_1',
-    instruction_2= '$instruction_2',
-    instruction_3= '$instruction_3',
-    instruction_4= '$instruction_4',
-    instruction_5= '$instruction_5',
-    instruction_6= '$instruction_6',
-    instruction_7= '$instruction_7',
-    instruction_8= '$instruction_8',
-    instruction_9= '$instruction_9',
-    instruction_10 = '$instruction_10'
-                       WHERE id = $id";
-    runQuery($query1);
-    runQuery($query2);
-    runQuery($query3);
-
-}
-
+/**
+ * @param $data
+ * @param $img
+ */
 function storeRecipe($data, $img)
 {
     $result = fetchAll("SELECT MAX(id) FROM recipes");
     $id = $result[0]['MAX(id)'] + 1;
-    $name = $data['name'];
-    $ingredient_1 = $data['ingredient_1'] ?? null;
-    $ingredient_2 = $data['ingredient_2'] ?? null;
-    $ingredient_3 = $data['ingredient_3'] ?? null;
-    $ingredient_4 = $data['ingredient_4'] ?? null;
-    $ingredient_5 = $data['ingredient_5'] ?? null;
-    $ingredient_6 = $data['ingredient_6'] ?? null;
-    $ingredient_7 = $data['ingredient_7'] ?? null;
-    $ingredient_8 = $data['ingredient_8'] ?? null;
-    $ingredient_9 = $data['ingredient_9'] ?? null;
-    $ingredient_10 = $data['ingredient_10'] ?? null;
-
-    $instruction_1 = $data['instruction_1'] ?? null;
-    $instruction_2 = $data['instruction_2'] ?? null;
-    $instruction_3 = $data['instruction_3'] ?? null;
-    $instruction_4 = $data['instruction_4'] ?? null;
-    $instruction_5 = $data['instruction_5'] ?? null;
-    $instruction_6 = $data['instruction_6'] ?? null;
-    $instruction_7 = $data['instruction_7'] ?? null;
-    $instruction_8 = $data['instruction_8'] ?? null;
-    $instruction_9 = $data['instruction_9'] ?? null;
-    $instruction_10 = $data['instruction_10'] ?? null;
-    echo "här:";
     $query1 = "INSERT INTO recipes VALUES(
     $id,
-    '$name',
-    '$img')";
+    :name,
+    :img)";
     $query2 = "INSERT INTO ingredients VALUES(
     $id,
-    '$ingredient_1',
-    '$ingredient_2',
-    '$ingredient_3',
-    '$ingredient_4',
-    '$ingredient_5',
-    '$ingredient_6',
-    '$ingredient_7',
-    '$ingredient_8',
-    '$ingredient_9',
-     '$ingredient_10')";
+    :ingredient_1,
+    :ingredient_2,
+    :ingredient_3,
+    :ingredient_4,
+    :ingredient_5,
+    :ingredient_6,
+    :ingredient_7,
+    :ingredient_8,
+    :ingredient_9,
+     :ingredient_10)";
     $query3 = "INSERT INTO instructions VALUES(
      $id,
-    '$instruction_1',
-    '$instruction_2',
-    '$instruction_3',
-    '$instruction_4',
-    '$instruction_5',
-    '$instruction_6',
-    '$instruction_7',
-    '$instruction_8',
-    '$instruction_9',
-    '$instruction_10')";
+    :instruction_1,
+    :instruction_2,
+    :instruction_3,
+    :instruction_4,
+    :instruction_5,
+    :instruction_6,
+    :instruction_7,
+    :instruction_8,
+    :instruction_9,
+    :instruction_10)";
 
-    runQuery($query1);
-    runQuery($query2);
-    runQuery($query3);
+    updateDatabase($query1, $query2, $query3, $data, $img);
 
+
+}
+
+
+/**
+ * @param $query1
+ * @param $query2
+ * @param $query3
+ * @param $data
+ * @param $img
+ */
+function updateDatabase($query1, $query2, $query3, $data, $img)
+{
+    $db = connect();
+    $stmt1 = $db->prepare($query1);
+    $stmt2 = $db->prepare($query2);
+    $stmt3 = $db->prepare($query3);
+    $stmt1->bindParam(":name", $data["name"]);
+    $stmt1->bindParam(":img", $img);
+    $stmt2->bindParam(":ingredient_1", $data["ingredient_1"]);
+    $stmt2->bindParam(":ingredient_2", $data["ingredient_2"]);
+    $stmt2->bindParam(":ingredient_3", $data["ingredient_3"]);
+    $stmt2->bindParam(":ingredient_4", $data["ingredient_4"]);
+    $stmt2->bindParam(":ingredient_5", $data["ingredient_5"]);
+    $stmt2->bindParam(":ingredient_6", $data["ingredient_6"]);
+    $stmt2->bindParam(":ingredient_7", $data["ingredient_7"]);
+    $stmt2->bindParam(":ingredient_8", $data["ingredient_8"]);
+    $stmt2->bindParam(":ingredient_9", $data["ingredient_9"]);
+    $stmt2->bindParam(":ingredient_10", $data["ingredient_10"]);
+    $stmt3->bindParam(":instruction_1", $data["instruction_1"]);
+    $stmt3->bindParam(":instruction_2", $data["instruction_2"]);
+    $stmt3->bindParam(":instruction_3", $data["instruction_3"]);
+    $stmt3->bindParam(":instruction_4", $data["instruction_4"]);
+    $stmt3->bindParam(":instruction_5", $data["instruction_5"]);
+    $stmt3->bindParam(":instruction_6", $data["instruction_6"]);
+    $stmt3->bindParam(":instruction_7", $data["instruction_7"]);
+    $stmt3->bindParam(":instruction_8", $data["instruction_8"]);
+    $stmt3->bindParam(":instruction_9", $data["instruction_9"]);
+    $stmt3->bindParam(":instruction_10", $data["instruction_10"]);
+    $stmt1->execute();
+    $stmt2->execute();
+    $stmt3->execute();
+}
+
+
+/**
+ * @param $id
+ * @param $data
+ * @param $img
+ */
+function updateRecipe($id, $data, $img)
+{
+
+    $query1 = "UPDATE recipes SET 
+                   name=:name,
+                   img = :img
+            WHERE id = $id";
+    $query2 = "UPDATE ingredients SET
+                       ingredient_1 = :ingredient_1,
+    ingredient_2= :ingredient_2,
+    ingredient_3= :ingredient_3,
+    ingredient_4= :ingredient_4,
+    ingredient_5= :ingredient_5,
+    ingredient_6= :ingredient_6,
+    ingredient_7= :ingredient_7,
+    ingredient_8= :ingredient_8,
+    ingredient_9= :ingredient_9,
+    ingredient_10 = :ingredient_10
+                       WHERE id = $id";
+    $query3 = "UPDATE instructions SET
+    instruction_1 = :instruction_1,
+    instruction_2= :instruction_2,
+    instruction_3= :instruction_3,
+    instruction_4= :instruction_4,
+    instruction_5= :instruction_5,
+    instruction_6= :instruction_6,
+    instruction_7= :instruction_7,
+    instruction_8= :instruction_8,
+    instruction_9= :instruction_9,
+    instruction_10 = :instruction_10
+                       WHERE id = $id";
+    updateDatabase($query1, $query2, $query3, $data, $img);
+
+}
+
+
+/**
+ * @param $post
+ * @param $files
+ * @return array
+ */
+function uploadFile($post, $files){
+    $test = "false";
+    if (isset($post['submit'])) {
+        $img = $files['img'];
+        $imgName = $files['img']['name'];
+        $imgTmpName = $files['img']['tmp_name'];
+        $imgSize = $files['img']['size'];
+        $imgError = $files['img']['error'];
+        $imgType = $files['img']['type'];
+
+        $imgExt = explode('.', $imgName);
+        $imgActualExt = strtolower(end($imgExt));
+
+        $allowed = array('jpg', 'jpeg', 'png');
+        if ($files["img"]["name"] != "") {
+            if (in_array($imgActualExt, $allowed)) {
+                if ($imgError === 0) {
+                    if ($imgSize < 5000000) {
+                        $test = "true";
+                        $fileNameNew = uniqid('', true) . "." . $imgActualExt;
+                        $fileDestination = 'uploads/' . $fileNameNew;
+                        move_uploaded_file($imgTmpName, $fileDestination);
+                    } else {
+                        echo "Din fil är för stor!";
+                        $fileNameNew = null;
+                    }
+                } else {
+                    echo "There was an error uploading your file!";
+                    $fileNameNew = null;
+                }
+            } else {
+                echo "Du kan inte ladda upp bilder i det här formatet! Kolla att din bild är en jpg, jpeg eller png";
+                $fileNameNew = null;
+            }
+        } else {
+            $test = "true";
+            $fileNameNew = null;
+        }
+    }
+    $result = array($test,$fileNameNew);
+    return  $result;
 }
--- views/editRecipe.view.php
+++ views/editRecipe.view.php
@@ -5,40 +5,104 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <title>Redigera <?php echo "hej" ?></title>
 </head>
 <body>
-<a href="../">Kom hem igen!</a>
-<h1>Receptsida</h1>
-<h2>Redigera <?=$recipe[0][0]["name"]?></h2>
-<?php
-?>
-<form action="../updateRecipe.php/?id=<?=$id?>" method="post" enctype="multipart/form-data">
-    <input type="text" name="name" id="name" value="<?= $recipe[0][0]['name'] ?>"> <br>
-    <?php if($recipe[0][0]["img"] != null){?>
-        <img class="card-img-top" src="../uploads/<?=$recipe[0][0]['img']?>" alt="Card image cap">
-    <?php } ?>
-     <br> <input type="file" name="img" id="img" value="<?=$recipe[0][0]['img']?>"> <br>
-    <?php
-    for ($i = 1; $i < 11; $i++) {
-        if ($recipe[1][0]['ingredient_' . $i] != "") {
-            ?>
-            <input type="text" name="ingredient_<?= $i ?>" id="<?= $i ?>"
-                   value="<?= $recipe[1][0]['ingredient_' . $i] ?>">
-            <br>
-        <?php }
-    }
-    for ($i = 1; $i < 11; $i++) {
-        if ($recipe[2][0]['instruction_' . $i] != "") {
-            ?>
-            <textarea name="instruction_<?= $i ?>" id="<?= $i ?>" rows="10" cols="70"><?=$recipe[2][0]["instruction_".$i]?></textarea>
-            <br>
-        <?php }
-    }
-    ?>
-    <button type="submit" name="submit">Spara ändringar</button>
+<div class="container">
+    <h1 class="display-3"><a class="text-dark" href="../">Receptsida</a></h1>
+    <div class="row">
 
-</form>
+        <div class="col align-self-center">
+            <h2>Redigera <?= $recipe[0][0]["name"] ?></h2>
+            <?php
+            ?>
+            <form action="../updateRecipe.php/?id=<?= $id ?>" method="post" enctype="multipart/form-data">
+                <input type="text" name="name" id="name" value="<?= $recipe[0][0]['name'] ?>"> <br>
+                <?php if ($recipe[0][0]["img"] != null) { ?>
+                    <img src="../uploads/<?= $recipe[0][0]['img'] ?>" alt="Card image cap">
+                <?php } ?>
+                <br> <input type="file" name="img" id="img" value="<?= $recipe[0][0]['img'] ?>"> <br>
+                <?php
+                for ($i = 1; $i < $countIngredients + 1; $i++) {
+                    ?>
+                    <input type="text" class="form-control" name="ingredient_<?= $i ?>" id="<?= $i ?>"
+                           value="<?= $recipe[1][0]['ingredient_' . $i] ?>">
+                    <br>
+                    <?php
+                } ?>
+
+                <!-- Lägg till( och ta bort) ingredienser-->
+                <div id="ingredientBox1"></div>
+                <div id="ingredientBox2"></div>
+                <div id="ingredientBox3"></div>
+                <div id="ingredientBox4"></div>
+                <div id="ingredientBox5"></div>
+                <div id="ingredientBox6"></div>
+                <div id="ingredientBox7"></div>
+                <div id="ingredientBox8"></div>
+                <div id="ingredientBox9"></div>
+                <div id="ingredientBox10"></div>
+
+                <input type="button" class="btn btn-secondary" onclick="addIngredient()" value="+"> <label for=""> Lägg
+                    till
+                    ingrediens</label>
+                <?php
+
+                for ($i = 1; $i < $countInstructions + 1; $i++) {
+                    ?>
+                    <textarea name="instruction_<?= $i ?>" id="<?= $i ?>" rows="10"
+                              cols="70"><?= $recipe[2][0]["instruction_" . $i] ?></textarea>
+                    <br>
+                    <?php
+                }
+                ?>
+
+                <!-- Lägg till( och ta bort) instruktioner-->
+                <div id="instructionBox1"></div>
+                <div id="instructionBox2"></div>
+                <div id="instructionBox3"></div>
+                <div id="instructionBox4"></div>
+                <div id="instructionBox5"></div>
+                <div id="instructionBox6"></div>
+                <div id="instructionBox7"></div>
+                <div id="instructionBox8"></div>
+                <div id="instructionBox9"></div>
+                <div id="instructionBox10"></div>
+                <input type="button" class="btn btn-secondary" onclick="addInstruction()" value="+"> <label for="">Lägg
+                    till
+                    instruktion</label>
+
+                <br>
+                <button type="submit" name="submit">Spara ändringar</button>
+
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+let amountIngredient = <?=$countIngredients?>;
+let amountInstruction = <?=$countInstructions?>;
+    function addIngredient() {
+        amountIngredient += 1;
+        if (amountIngredient < 11) {
+            document.getElementById("ingredientBox" + amountIngredient).innerHTML += "<label>Ingrediens " + amountIngredient + ": <input type='text' class='form-control' name='ingredient_" + amountIngredient + "' id='ingredient_" + amountIngredient + "' ></label> <br>";
+        } else {
+            alert("Du kan inte ha mer än 10 ingredienser!")
+        }
+    }
+
+    function addInstruction() {
+        amountInstruction += 1;
+        if (amountInstruction < 11) {
+            document.getElementById("instructionBox" + amountInstruction).innerHTML += "<label>Steg " + amountInstruction + ":</label><br> <textarea type='text' class='form-control' name='instruction_" + amountInstruction + "' id='instruction_" + amountInstruction + "'  rows='5'></textarea> <br>";
+        } else {
+            alert("Du kan inte ha mer än 10 instruktioner!")
+        }
+    }
+</script>
 </body>
 </html>
 
