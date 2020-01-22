11111861daaa88c8dd41b7a0aef55aac65c1fb80
alicelindholm <54351014+alicelindholm@users.noreply.github.com>
2020-01-22 13:17:51 +0100

    commit

--- editRecipe.php
+++ editRecipe.php
@@ -1,4 +1,3 @@
-<script src="main.js"></script>
 <?php
 require "db.php";
 var_dump($vars);
@@ -28,27 +27,6 @@ function countNotEmpty($value, $number, $recipe)
 require "views/editRecipe.view.php"; ?>
 
 
-<!--<script>
-   // let amountIngredient = <?=$countIngredients?>;
-   // let amountInstruction = <?=$countInstructions?>;
-
-/*    function addIngredient() {
-        amountIngredient += 1;
-        if (amountIngredient < 11) {
-            document.getElementById("ingredientBox" + amountIngredient).innerHTML += "<label>Ingrediens "+amountIngredient+":<input type='text' class='form-control' name='ingredient_" + amountIngredient + "' id='ingredient_" + amountIngredient + "' ></label> <br>";
-        } else {
-            alert("Du kan inte ha mer än 10 ingredienser!")
-        }
-    }
-
-    function addInstruction() {
-        amountInstruction += 1;
-        if (amountInstruction < 11) {
-            document.getElementById("instructionBox" + amountInstruction).innerHTML += "<label>Steg "+amountInstruction+":<br> <textarea type='text' class='form-control' name='instruction_" + amountInstruction + "' id='instruction_" + amountInstruction + "' rows='5' cols='50'></textarea> </label> <br>";
-        } else {
-            alert("Du kan inte ha mer än 10 instruktioner!")
-        }
-    } */
-</script> !-->
+<script src="main.js"></script>
 
 
