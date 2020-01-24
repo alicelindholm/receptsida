2ff237998992971809658925897ec8a06761874b
alicelindholm <54351014+alicelindholm@users.noreply.github.com>
2020-01-24 09:57:22 +0100

    commit

--- views/editRecipe.view.php
+++ views/editRecipe.view.php
@@ -7,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <title>Redigera <?php echo "hej" ?></title>
+    <title>Redigera</title>
+    <link rel="icon" href="../icon.jpg">
 </head>
 <body>
 <div class="container">
